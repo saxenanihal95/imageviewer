db41c37c4797c53c2a77d1cbd933a84a134cbc02
saxenanihal95 <nsaxena@localhost.localdomain>
2018-03-05 07:49:39 +0530

    added ApiService

---
+++ app/src/main/java/com/example/nsaxena/imageviewer/ApiService.java
@@ -0,0 +1,15 @@
+package com.example.nsaxena.imageviewer;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+/**
+ * Created by nsaxena on 5/3/18.
+ */
+
+public interface ApiService {
+    @GET("/json")
+    Observable<List<Image>> getproductdata();
+}
