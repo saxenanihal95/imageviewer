acb22b6d1cb8f20ffa4500fc55bdc5eb0a100248
saxenanihal95 <nsaxena@localhost.localdomain>
2018-03-05 19:57:19 +0530

    added country detail activity

---
+++ app/src/main/java/com/example/nsaxena/imageviewer/CountryDetail.java
@@ -0,0 +1,13 @@
+package com.example.nsaxena.imageviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountryDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_detail);
+    }
+}
--- app/src/main/java/com/example/nsaxena/imageviewer/MainActivity.java
+++ app/src/main/java/com/example/nsaxena/imageviewer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nsaxena.imageviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -89,7 +90,8 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onNext(Country country) {
-
+                        Intent intent=new Intent(getApplicationContext(),CountryDetail.class);
+                        startActivity(intent);
                     }
 
                     @Override
